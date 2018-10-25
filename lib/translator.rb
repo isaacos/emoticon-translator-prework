ef1fc0939162ddfaca626ab0e69293c5232c9036
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-25 00:14:26 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,14 @@
 # require modules here
 
-def load_library
+require "yaml"
+
+def load_library (file_path)
   # code goes here
+  
+ file_path = YAML.load_file('lib/emoticons.yml')
+     file_path
+  
+  
 end
 
 def get_japanese_emoticon
